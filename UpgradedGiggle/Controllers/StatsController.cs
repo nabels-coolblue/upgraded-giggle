439d867f66165d55469ee1e99891f695efa30eec
Niels Abels <n.abels@coolblue.nl>
2016-08-05 11:59:33 +0200

    Added sample graph based on WebApi controller output.

---
+++ UpgradedGiggle/Controllers/StatsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UpgradedGiggle.Models;
+
+namespace UpgradedGiggle.Controllers
+{
+    public class StatsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var users = new List<User>();
+
+            var random = new Random();
+
+            users.Add(new User() { Name = "tangent", NumberOfLines = random.Next(1, 2048) });
+            users.Add(new User() { Name = "alpha", NumberOfLines = random.Next(1, 2048) });
+            users.Add(new User() { Name = "azimuth", NumberOfLines = random.Next(1, 2048) });
+            users.Add(new User() { Name = "literal", NumberOfLines = random.Next(1, 2048) });
+            
+            return Json(
+                new string[][] {
+                    users.Select(user => user.Name).ToArray(),
+                    users.Select(user => user.NumberOfLines.ToString()).ToArray(),
+                }
+            );
+        }
+    }
+}
